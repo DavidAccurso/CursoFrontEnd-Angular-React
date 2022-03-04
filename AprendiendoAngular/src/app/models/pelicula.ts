64bcee0d168fcbd980e457035899214c6b0c7677
David Accurso <davidmaccurso@gmail.com>
2022-03-04 12:19:32 -0300

    Creado modelo peliculas, slider small funcionando

--- AprendiendoAngular/src/app/models/pelicula.ts
+++ AprendiendoAngular/src/app/models/pelicula.ts
@@ -1,12 +1,19 @@
 export class Pelicula {
     
-    public title: string;
-    public image: string;
-    public year: number;
+    // public title: string;
+    // public image: string;
+    // public year: number;
 
-    constructor(title: string, image: string, year: number) {
-        this.title = title;
-        this.image = image;
-        this.year = year;
-    }
+    // constructor(title: string, image: string, year: number) {
+    //     this.title = title;
+    //     this.image = image;
+    //     this.year = year;
+    // }
+
+    // mejor forma de crear props y asignarlas
+    constructor(
+        public title: string,
+        public image: string,
+        public year: number
+    ){}
 }
